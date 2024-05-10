4ee8695423aa2ac8178b96dc9ae49524e4269355
AndryAditama <145452892+AndryAditama@users.noreply.github.com>
2024-05-10 09:33:49 +0700

    Add files

---
+++ script.js
@@ -0,0 +1,9 @@
+var toggle = document.querySelector(".toggle-wrapper");
+var navBar = document.querySelector(".navbar");
+
+
+toggle.addEventListener('click', function() {
+    
+        navBar.setAttribute("class", "navbar-off");
+    
+})
